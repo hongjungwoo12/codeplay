34cd0bf288fa041b165aa360602134ced91fce61
hongjungwoo12 <hongjungwoo0405@gmail.com>
2023-04-05 18:26:23 +0900

    04/05 슬라이싱 까지완료

--- basic/01_string.py
+++ basic/01_string.py
@@ -1,20 +1,60 @@
-name = ""
-gender = ""
-age = 0
+# name = ""
+# gender = ""
+# age = 0
 
-# print(name, len(name), age - 10)
+# # print(name, len(name), age - 10)
 
-name = input("이름을 입력하세요 : ")
-gender = input("성별을 입력하시오(남/여 둘 중 하나만 골라)")
-age = input ("나이를 입력하세요 : ")
+# name = input("이름을 입력하세요 : ")
+# gender = input("성별을 입력하시오(남/여 둘 중 하나만 골라)")
+# age = input ("나이를 입력하세요 : ")
 
-if gender == "남":
-    gender=" Mr."
-elif gender == "여":
-    gender = "Ms."
-print(f"{gender}{name[0]}님 반갑습니다. 지옥에 오신것을 환영합니다!")
+# if gender == "남":
+#     gender=" Mr."
+# elif gender == "여":
+#     gender = "Ms."
+# print(f"{gender}{name[0]}님 반갑습니다. 지옥에 오신것을 환영합니다!")
 
-if int(age) <= 18:
-    print("당신은 미성년자시군요 미성년자 전용 건전한 지옥으로 입장하세요.")
-elif int(age) >= 19:
-    print("당신은 성인이시군요. 성인용 레알살벌한 지옥으로 입장하세요.")
+# if int(age) <= 18:
+#     print("당신은 미성년자시군요 미성년자 전용 건전한 지옥으로 입장하세요.")
+# elif int(age) >= 19:
+    # print("당신은 성인이시군요. 성인용 레알살벌한 지옥으로 입장하세요.")
+
+
+# eng = "abcdefg"
+# kor = "가나다라마바사"
+
+# print(eng[3])
+# print(f"{kor[-1:-3:-1]}! 이 자슥아!")
+# print(eng[::-1])
+
+# name =""
+# age = 0
+
+# name = input("이름은? : ")
+# age = input("나이는? : ")
+
+# if int(age) < 16:
+#     print(f"{name[1:]} 너 까불지마 임마")
+# else:
+#     print(f"{name[0]}사장님 느무 믓지시다~ 굽신굽신~")
+
+import random
+
+name = "못생긴코딩쌤"
+call = ["바보", "천재", "미남", "추남", "또라이"]
+print(f"역시! {name[3:]} {call[random.randint(0, 4)]}!")
+
+
+ㅋㅋㄹㅃㅃ
+
+
+
+
+
+
+
+
+
+
+
+
